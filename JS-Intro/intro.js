cf4dc55a9a16257d477ec72635ac81a2fe0b721b
Arun Singh <arunsinghsnd@gmail.com>
2025-07-23 23:03:06 +0530

    intro to JS

---
+++ JS-Intro/intro.js
@@ -0,0 +1,15 @@
+/**
+ *
+ *  Websites
+ *  Web and Mobile apps
+ *  Desktop apps
+ *  Game Development
+ *  Single Page applications
+ *  Data Visualization
+ *  Interactive UX
+ *  Plugins for Popular Applications
+ *  Serverless functions
+ *  Artificial Intelligence
+ *  Virtul Reality Apps
+ * 
+ */
