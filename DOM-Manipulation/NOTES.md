d3cd0337cc2a69eb967b9e7777505b61f012eb87
Arun Singh <arunsinghsnd@gmail.com>
2025-10-23 18:30:38 +0530

    add the nodelist code and comments

---
+++ DOM-Manipulation/NOTES.md
@@ -0,0 +1,27 @@
+Window Objects
+
+DOM Objects
+
+- Changing page content (text, HTML)
+- Changing element styles (colors, fonts, size)
+- Handling user events (clicks, typing, scrolling)
+
+BOM Browser Features
+
+- Alerts
+- Confirmations
+- Promts
+- Browser window size
+- URL, history navigation
+- Timers
+
+JS core
+
+- Variables and Data Types
+- Operators
+- Functions
+- Objects and Arrays
+
+## Properties and Methods and Node these are the most important things
+
+-> Tags -> Token -> Node
--- DOM-Manipulation/script.js
+++ DOM-Manipulation/script.js
@@ -16,54 +16,66 @@ Document object model
 */
 
 // this the way of selecting the DOM
-let h1 = document.querySelector("h1");
+// let h1 = document.querySelector("h1");
 
-h1.addEventListener("mouseenter", () => {
-  h1.style.backgroundColor = "red";
-  h1.style.cursor = "pointer";
-  console.log("Added the event listener on the h1 tag");
-});
+// h1.addEventListener("mouseenter", () => {
+//   h1.style.backgroundColor = "red";
+//   h1.style.cursor = "pointer";
+//   console.log("Added the event listener on the h1 tag");
+// });
 
-h1.addEventListener("mouseleave", () => {
-  h1.style.backgroundColor = "green";
-  console.log("Added the event listener on the h1 tag");
-});
+// h1.addEventListener("mouseleave", () => {
+//   h1.style.backgroundColor = "green";
+//   console.log("Added the event listener on the h1 tag");
+// });
 
-h1.style.backgroundColor = "green";
+// h1.style.backgroundColor = "green";
 
 // Here we are change the content
-let innerHTML = (h1.innerHTML = "I am doing DOM Manipulation");
-console.log("innerHTML", innerHTML);
-let textContent = (h1.textContent = "Hello from the textContent");
-console.log("textContent", textContent);
+// let innerHTML = (h1.innerHTML = "I am doing DOM Manipulation");
+// console.log("innerHTML", innerHTML);
+// let textContent = (h1.textContent = "Hello from the textContent");
+// console.log("textContent", textContent);
 
-console.log(h1);
+// console.log(h1);
 
-let b = document.querySelector("h2");
-b.style.backgroundColor = "yellow";
+// let b = document.querySelector("h2");
+// b.style.backgroundColor = "yellow";
 
-console.log(b);
+// console.log(b);
 
 // Event listener
 
-const btn = document.getElementById("callBtn");
-function btnCall() {
-  btn.addEventListener("click", (event) => {
-    console.log("Hello");
-  });
-}
+// const btn = document.getElementById("callBtn");
+// function btnCall() {
+//   btn.addEventListener("click", (event) => {
+//     // console.log("Hello");
+//   });
+// }
 
-let box = document.getElementById("box");
+// let box = document.getElementById("box");
 
-let magicBtnExpand = document.getElementById("magicBtnExpand");
-let magicBtnShrink = document.getElementById("magicBtnShrink");
+// let magicBtnExpand = document.getElementById("magicBtnExpand");
+// let magicBtnShrink = document.getElementById("magicBtnShrink");
 
-magicBtnExpand.addEventListener("click", () => {
-  box.style.backgroundColor = "blue";
-  box.style.scale = "1.0";
-});
+// magicBtnExpand.addEventListener("click", () => {
+//   box.style.backgroundColor = "blue";
+//   box.style.scale = "1.0";
+// });
 
-magicBtnShrink.addEventListener("click", () => {
-  box.style.backgroundColor = "blue";
-  box.style.scale = "0.5";
-});
+// magicBtnShrink.addEventListener("click", () => {
+//   box.style.backgroundColor = "blue";
+//   box.style.scale = "0.5";
+// });
+
+// console.log(window.alert("Hello, Arun Kumar Singh"));
+
+// console.log(document);
+// console.dir(document);
+
+let nodeList = document.querySelectorAll("div");
+
+console.log(nodeList);
+
+// this is list of all the node ka list
+// type of array
