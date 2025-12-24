0bd88090a72c5e51b343399ca052dc37c51a98b2
Arun Singh <arunsinghsnd@gmail.com>
2025-12-24 23:30:30 +0530

    added the polyfill of bind

---
+++ PolyFillForBind.js
@@ -0,0 +1,27 @@
+// General by Default in built Bind function imlimation
+
+const person = {
+  name: "Arun",
+};
+
+function greet(city, country) {
+  console.log(
+    "Hello, " + "I am " + this.name + " from " + city + " " + country
+  );
+}
+
+const boundGreet = greet.bind(person);
+boundGreet();
+
+// Write the polyfill for Bind which is own implimentation
+
+Function.prototype.myBind = function (context, ...args) {
+  const obj = this;
+
+  return function (...argsCall) {
+    return obj.apply(context, [...args, argsCall]);
+  };
+};
+
+const myBindboundGreet = greet.myBind(person, "Hyderabad");
+myBindboundGreet("India");
