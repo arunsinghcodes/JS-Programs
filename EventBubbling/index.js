4df2470c0abd5d0f79b8d2fabb2923d8dd72b836
Arun Singh <arunsinghsnd@gmail.com>
2026-03-06 21:42:00 +0530

    Added the eventbubbling

---
+++ EventBubbling/index.js
@@ -0,0 +1,13 @@
+document.querySelector("#grandParent").addEventListener("click", () =>{
+    console.log("GrandParent Clicked")
+}, true)
+
+
+document.querySelector("#parent").addEventListener("click", () =>{
+    console.log("parent Clicked")
+}, true)
+
+
+document.querySelector("#child").addEventListener("click", () =>{
+    console.log("child Clicked")
+}, true)
