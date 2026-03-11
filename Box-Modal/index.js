28a56a3792747def7d24fb2e13777ae70a6f39e6
Arun Singh <arunsinghsnd@gmail.com>
2026-03-11 06:05:58 +0530

    Add html Js example

---
+++ Box-Modal/index.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+
+// 1st Approach
+
+const changeHandler = () =>{
+    document.querySelector(".btn").innerText = "Loading...";
+}
+
+// 2nd Approach
+
+const btnTwo = document.querySelector(".btnTwo");
+
+const changeHandlerSecond = (btnTwo) =>{
+    btnTwo.innerText = "Loading..."
+}
+
+// 3rd Approach
+
+const btnThree = document.querySelector(".btnThree");
+
+btnThree.addEventListener("click", () =>{
+    btnThree.innerText = "Loading..."
+})
