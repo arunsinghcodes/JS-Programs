857a8673fd5e9a2ff8564674dcc5fbb6df461745
Arun Singh <arunsinghsnd@gmail.com>
2026-03-14 20:39:02 +0530

    Added the  fetch and Axios 2nd compare

---
+++ fetchVsAxios/two.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// While POST call diff with them
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    title: "New Post",
+    body: "This is new POST",
+    userId: 2323,
+  }),
+})
+  .then((repsonse) => repsonse.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error.message));
+
+axios
+  .post("https://jsonplaceholder.typicode.com/posts", {
+    title: "New Post with Axois",
+    body: "This is new with Axois",
+    userId: 23233,
+  })
+  .then((data) => console.log(data.data))
+  .catch((error) => console.error("Error:", error.message));
