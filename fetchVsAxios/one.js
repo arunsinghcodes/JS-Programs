0edf4e71aa4d849ca49ea43effc47aad66e95bb0
Arun Singh <arunsinghsnd@gmail.com>
2026-03-14 20:27:10 +0530

    Added the  fetch and Axios 1st compare

---
+++ fetchVsAxios/one.js
@@ -0,0 +1,16 @@
+// fetch vs axios
+
+import axios from "axios";
+
+// fetch requires .json() to parse the response.
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+// Axios Automatically parse JSON
+axios
+  .get("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => console.log(response))
+  .catch((error) => console.log("Error:", error));
